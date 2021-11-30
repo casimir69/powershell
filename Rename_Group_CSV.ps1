cdd80cf8e4c0059d7b53a0031a9bf63ad3ef8272
casimir69 <65390071+casimir69@users.noreply.github.com>
2021-11-30 10:34:15 +0100

    Update Rename_Group_CSV.ps1

--- Rename_Group_CSV.ps1
+++ Rename_Group_CSV.ps1
@@ -1,6 +1,13 @@
-#
-#    Rename_Group_CSV.ps1
-#
+<#
+.SYNOPSIS
+        Ce script Powershell permet de renommer en masse des groupes Active Directory
+	
+.DESCRIPTION
+	Ce script Powershell va renommer des groupes AD en se basant sur un fichier ".csv" en entrée ($OldName,$NewName,$Description)
+
+.NOTES
+	Ce script a besoin d'avoir des privilèges sur le domaine sur lequel on intervient
+#>
 
 $ImportGroups = Import-Csv "RenameGroups.csv"
 
@@ -8,12 +15,13 @@ foreach ($Group in $ImportGroups)
 {
     $OldName = $Group.OldName
     $NewName = $Group.NewName
+    $NewDescription = $Group.NewDescription
     $GroupInAD = Get-ADGroup $Group.OldName
 	
     try
    {
         "In try: working on $OldName"
-        Set-ADGroup -Identity $GroupInAD -SamAccountName $NewName
+        Set-ADGroup -Identity $GroupInAD -SamAccountName $NewName -Description $Group.NewDescription
         Rename-ADObject -Identity $GroupInAD -NewName $NewName
         Write-Output ($OldName + " has been renamed to " + $NewName)
     }
