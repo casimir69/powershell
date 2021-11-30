5897135cac9f7e3e952800b4c70805ea07b4d82e
casimir69 <65390071+casimir69@users.noreply.github.com>
2021-11-30 10:07:14 +0100

    Create Rename_Group_CSV.ps1

---
+++ Rename_Group_CSV.ps1
@@ -0,0 +1,26 @@
+#
+#    Rename_Group_CSV.ps1
+#
+
+$ImportGroups = Import-Csv "RenameGroups.csv"
+
+foreach ($Group in $ImportGroups)
+{
+  $OldName = $Group.OldName
+	$NewName = $Group.NewName
+	$GroupInAD = Get-ADGroup $Group.OldName
+	
+    try
+   {
+   		  "In try: working on $OldName"
+        Set-ADGroup -Identity $GroupInAD -SamAccountName $NewName
+        Rename-ADObject -Identity $GroupInAD -NewName $NewName
+        Write-Output ($OldName + " has been renamed to " + $NewName)
+    }
+
+    catch
+    {
+		"in Catch for $TempOldName"
+        Write-Output "Error: $_"
+    }
+}
