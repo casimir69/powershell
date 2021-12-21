1e084f4e711aab4d01dee57a0cc4387477ed3f82
casimir69 <65390071+casimir69@users.noreply.github.com>
2021-12-21 17:39:56 +0100

    Update ScanForLog4jOnWin.ps1

--- ScanForLog4jOnWin.ps1
+++ ScanForLog4jOnWin.ps1
@@ -3,8 +3,9 @@ $drives = Get-PSDrive -PSProvider FileSystem
 Write-Host --== To Begin ==--
 Forearch ($drive in $drives)
   {
-  Write-Host Scan "($drive.Name):\"
-  Get-Children $drive.Name -Recurse -Force -Include *log4j*.jar -ErrorAction SilentlyContinue
+  $drove = $drive.Name +":\"
+  Write-Host "Scan"+ $drove +"volume"
+  Get-Children $drove -Recurse -Force -Include *log4j*.jar -ErrorAction SilentlyContinue
   }
   
   Write-Host --== The End ==--
