eb7395afb389921163741480c31624ca2c58cb08
casimir69 <65390071+casimir69@users.noreply.github.com>
2021-12-22 15:35:04 +0100

    Update ScanForLog4jOnWin.ps1

--- ScanForLog4jOnWin.ps1
+++ ScanForLog4jOnWin.ps1
@@ -1,10 +1,37 @@
+$execDateTime = Get-Date
+$localComputerName = $(Get-WmiObject -Class Win32_ComputerSystem).Name
+$fqdnDomainName = $(Get-WmiObject -Class Win32_ComputerSystem).Domain
+$fqdnLocalComputer = $localComputerName + "." + $fqdnDomainName
+$scriptFullPath = $MyInvocation.MyCommand.Definition
+$cmdLineUsed = $MyInvocation.Line
 $drives = Get-PSDrive -PSProvider FileSystem
+$DateTime = Get-date -Format "[dd/MM/yyyy][HH:mm:ss]"
 
-Write-Host --== To Start ==--
+$logfile = $scriptFullPath\ScanForLog4jOnWin.log
+Clear-Content $logfile
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $logfile -value $logstring
+}
+
+LogWrite ""
+LogWrite "Date/Time   ......................: $execDateTime"
+LogWrite "Local Computer....................: $fqdnLocalComputer"
+LogWrite ""
+LogWrite "Script Full Path..................: $scriptFullPath"
+LogWrite "Script Command Line Used..........: $cmdLineUsed"
+LogWrite ""
+LogWrite "Start Script in "$fqdnLocalComputer""
+
 Foreach ($drive in $drives)
   {
   $drove = $drive.Name +":\"
-  Write-Host "Scan" $drove "volume"
+  LogWrite "Scan" $drove "volume"
+  #Get-ChildItem $drove -Recurse -Force -Include regedit.exe -ErrorAction SilentlyContinue
   Get-ChildItem $drove -Recurse -Force -Include *log4j*.jar -ErrorAction SilentlyContinue
   }
-    Write-Host --== The End ==--
+  
+  LogWrite "The End"
