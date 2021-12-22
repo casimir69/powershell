174a94882cb034ee029764e6fcaf818e28d73141
casimir69 <65390071+casimir69@users.noreply.github.com>
2021-12-22 17:25:23 +0100

    Update ScanForLog4jOnWin.ps1

--- ScanForLog4jOnWin.ps1
+++ ScanForLog4jOnWin.ps1
@@ -3,35 +3,25 @@ $localComputerName = $(Get-WmiObject -Class Win32_ComputerSystem).Name
 $fqdnDomainName = $(Get-WmiObject -Class Win32_ComputerSystem).Domain
 $fqdnLocalComputer = $localComputerName + "." + $fqdnDomainName
 $scriptFullPath = $MyInvocation.MyCommand.Definition
-$cmdLineUsed = $MyInvocation.Line
 $drives = Get-PSDrive -PSProvider FileSystem
-$DateTime = Get-date -Format "[dd/MM/yyyy][HH:mm:ss]"
+$logfile = ("C:\Temp\ScanForLog4jOnWin_"+(Get-date -Format 'yyyy-MM-dd')+".log")
 
-$logfile = $scriptFullPath\ScanForLog4jOnWin.log
 Clear-Content $logfile
 
-Function LogWrite
-{
-   Param ([string]$logstring)
-
-   Add-content $logfile -value $logstring
-}
-
-LogWrite ""
-LogWrite "Date/Time   ......................: $execDateTime"
-LogWrite "Local Computer....................: $fqdnLocalComputer"
-LogWrite ""
-LogWrite "Script Full Path..................: $scriptFullPath"
-LogWrite "Script Command Line Used..........: $cmdLineUsed"
-LogWrite ""
-LogWrite "Start Script in "$fqdnLocalComputer""
+Add-Content $logfile "Début du script..................."
+Add-Content $logfile ".................................."
+Add-Content $logfile "Date/Time.........................: $execDateTime"
+Add-Content $logfile "Local Computer....................: $fqdnLocalComputer"
+Add-Content $logfile "Domain Name.......................: $fqdnDomainName"
+Add-Content $logfile "Script Full Path..................: $scriptFullPath"
+Add-Content $logfile ""
 
 Foreach ($drive in $drives)
   {
-  $drove = $drive.Name +":\"
-  LogWrite "Scan" $drove "volume"
-  #Get-ChildItem $drove -Recurse -Force -Include regedit.exe -ErrorAction SilentlyContinue
-  Get-ChildItem $drove -Recurse -Force -Include *log4j*.jar -ErrorAction SilentlyContinue
+  [string]$drove = $drive.Name +':\'
+  Add-Content $logfile "Scan $drove"
+  Get-ChildItem $drove -Recurse -Force -Include *log4j*.jar -ErrorAction SilentlyContinue | Add-Content $logfile
   }
-  
-  LogWrite "The End"
+  Add-Content $logfile "Fin du script...................."
+
+  Move-Item -Path $logfile -Destination \\$fqdnDomainName\SYSVOL\$fqdnDomainName\scripts\Log4j\
