dd2efe13bafa0c0f1a003a9d637f17f6da27d044
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-01-13 15:50:48 +0100

    Create Add_DNS_Record-A.ps1

---
+++ Add_DNS_Record-A.ps1
@@ -0,0 +1,29 @@
+Import-Module ActiveDirectory
+[string]$version = "13/01/2022_15:40"
+$fqdnDomainName = $(Get-WmiObject -Class Win32_ComputerSystem).Domain
+$records = Import-Csv -Path "add_to_dns_TIH.csv" -Delimiter ";"
+Write-Host ""
+Write-Host "################## DEBUT_DU_TRAITEMENT ####################"
+Write-Host "#             version : $version                  #"
+Write-Host "#      fqdnDomainName : $fqdnDomainName                     #"
+Write-Host "#    Attention, pas de guillements dans le fichier .csv   #"
+Write-Host "###########################################################"
+Write-Host ""
+
+foreach ($record in $records)
+    {
+    #$exist = Get-DnsServerResourceRecord -Zonename $fqdnDomainName -RRtype A -Name $record.computer -ErrorAction SilentlyContinue
+    #Write-Host $exist
+    #if ($exist -eq $false)
+     #   {
+        Write-Host "Ajout DNS - Record A de la machine "$record.computer" en "$record.ip""
+        Add-DnsServerResourceRecordA -Name $record.computer -IPv4Address $record.ip -ZoneName $fqdnDomainName -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 1
+        #Add-DnsServerResourceRecordPtr -ComputerName "svwltada.step-a.edf.fr" 
+      #  }
+       # else
+        #    {
+         #   Write-Host "Machine "$record.computer" est déjà dans le DNS"
+          #  }
+    }
+    Write-Host "#################### FIN_DU_TRAITEMENT ####################"
