187466cba7fa03fb05d43d3562b3b8dc630f8325
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-03-15 15:21:07 +0100

    Update Del_User_From_Group.ps1

--- Del_User_From_Group.ps1
+++ Del_User_From_Group.ps1
@@ -1,5 +1,25 @@
+<#
+.SYNOPSIS
+        Ce script Powershell permet de supprimer des users d'un groupe Active Directory en vérifiant sa présence au préalable
+.DESCRIPTION
+	Ce script Powershell 
+.EXAMPLE
+	n/c
+.NOTES
+	Ce script n'a pas besoin de permissions particulière
+#>
+$ErrorActionPreference = "Stop"
 
-Import-Module ActiveDirectory
+If ((Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue) -eq $null)
+{
+    Try {
+        Import-Module ActiveDirectory
+    } Catch {
+        Write-Error "Unable to load the module" -ErrorAction Continue
+        Write-Error $Error[1] -ErrorAction Continue
+        Exit 1
+    }
+}
 
 $GroupBefore = 
 $GroupAfter = 
@@ -18,3 +38,4 @@ ForEach ($User in $ListeUsers)
           }
           
   }
+Write-Host Terminé
