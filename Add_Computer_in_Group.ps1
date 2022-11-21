236de66ec34dc9f2fe1c92b9870b19f801e8add0
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-11-21 20:56:44 +0100

    Update Add_Computer_in_Group.ps1

--- Add_Computer_in_Group.ps1
+++ Add_Computer_in_Group.ps1
@@ -4,18 +4,17 @@
 .DESCRIPTION
 	Ce script Powershell va ajouter les computer que vous aurez préalablement indiqué dans un fichier .txt dans un groupe Active Directory de votre choix.
 	Nous aurons besoin du DN du groupe cible
-.EXAMPLE
-	n/c
-.NOTES
-	Ce script n'a pas besoin de permissions particulière
 #>
 $ErrorActionPreference = "Stop"
 
 If ((Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue) -eq $null)
 {
-    Try {
+    Try
+    {
         Import-Module ActiveDirectory
-    } Catch {
+    }
+    Catch
+    {
         Write-Error "Unable to load the module" -ErrorAction Continue
         Write-Error $Error[1] -ErrorAction Continue
         Exit 1
@@ -30,7 +29,8 @@ $nbrposteav = (Get-ADGroup $group -Properties member).member.Count
 Write-Host "---------------Debut de l'ajout---------------------"
 Write-Host $nbrposte" postes avant la mise à jour"
 
-Foreach($poste in $listeposte){
+Foreach($poste in $listeposte)
+{
     $computer = (Get-ADComputer $poste).DistinguishedName
     if ($computer.MemberOf)
     if (Get-ADGroupMember -Id $group | $_.
@@ -38,7 +38,7 @@ Foreach($poste in $listeposte){
     $pdt = Get-ADComputer $poste | select -ExpandProperty SamAccountName
     Write-Host "Ajout du SamAccountName "$pdt
     Add-ADGroupMember -Identity $group -Members $pdt -Server domain.fr #Attention la commande fonctionne avec le SamAccountName du poste et pas avec son DisplayName
-    }
+}
 
 $nbrposteap = (Get-ADGroup $group -Properties member).member.Count
 Write-Host $nbrposteap" postes après la mise à jour"
