e4e1e398640607987be5e5bb64d1c44496709353
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-11-21 20:42:40 +0100

    Update Add_DNS_Record-A.ps1

--- Add_DNS_Record-A.ps1
+++ Add_DNS_Record-A.ps1
@@ -10,20 +10,20 @@ Write-Host "#    Attention, pas de guillements dans le fichier .csv   #"
 Write-Host "###########################################################"
 Write-Host ""
 
-ForEach ($record in $records)
-    {
+Foreach ($record in $records)
+{
     #$exist = Get-DnsServerResourceRecord -Zonename $fqdnDomainName -RRtype A -Name $record.computer -ErrorAction SilentlyContinue
     #Write-Host $exist
     #if ($exist -eq $false)
-     #   {
+    #{
         Write-Host "Ajout DNS - Record A de la machine "$record.computer" en "$record.ip"
         Add-DnsServerResourceRecordA -Name $record.computer -IPv4Address $record.ip -ZoneName $fqdnDomainName -ErrorAction SilentlyContinue
         Start-Sleep -Seconds 1
         #Add-DnsServerResourceRecordPtr -ComputerName "DnsServer" 
-      #  }
-       # else
-        #    {
-         #   Write-Host "Machine "$record.computer" est déjà dans le DNS"
-          #  }
-    }
-    Write-Host "#################### FIN_DU_TRAITEMENT ####################"
+    #}
+    #else
+    #{
+    #   Write-Host "Machine "$record.computer" est déjà dans le DNS"
+    #}
+}
+Write-Host "#################### FIN_DU_TRAITEMENT ####################"
