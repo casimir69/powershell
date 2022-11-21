4e5aee040a781ea014cb46ac0495081ba8c8b777
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-11-21 20:40:26 +0100

    Update Check_Accounts-NeverExpire.ps1

--- Check_Accounts-NeverExpire.ps1
+++ Check_Accounts-NeverExpire.ps1
@@ -8,17 +8,19 @@
 #>
 
 ##### module #####
-If ((Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue) -eq $null )
+If ((Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue) -eq $null)
+{
+    Try
     {
-    Try {
         Import-Module ActiveDirectory
-        }
-        Catch {
-              Write-Error "Unable to load the module" -ErrorAction Continue
-              Write-Error $Error[1] -ErrorAction Continue
-              Exit 1
-              }
     }
+    Catch
+    {
+        Write-Error "Unable to load the module" -ErrorAction Continue
+        Write-Error $Error[1] -ErrorAction Continue
+        Exit 1
+    }
+}
 
 ##### variable #####
 [string]$scriptVersion = "v20220919"
@@ -36,7 +38,7 @@ Write-Host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] ### Début du script Che
 
 ##### fonction #####
 Function Mail
-    {
+{
     param(
         [parameter(Mandatory)]
         [string]$subject,
@@ -53,32 +55,32 @@ Function Mail
     Write-Host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] Sendind Report Email from $from to $to"
 
     Send-MailMessage -SmtpServer $smtpServer -Port $port -From $from -To $to -Bcc $bcc -Subject $subject -Body $body -bodyasHTML -Attachments $logfile -Encoding $mailEncoding
-    }
+}
 
 ##### script #####
 foreach ($user IN $users)
-    {
+{
     [string]$name = $user.SamAccountName
     [bool]$state = $user.PasswordNeverExpires
     $nbrAccount++
     if ($state -like "$true")
-        {
+    {
         $nbrAccountNE++
         Add-Content $logfile "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] Le compte $name sur $domain a son mot de passe configuré en Never-Expire, merci de supprimer cette option."
         Write-Host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] Le compte $name sur $domain a son mot de passe configuré en Never-Expire, merci de supprimer cette option." -BackgroundColor DarkRed
-        }
+    }
         elseif ($state -like "$false")
-            {
+        {
             Add-Content $logfile "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] Le compte $name sur $domain est conforme aux exigences."
             Write-host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] Le compte $name sur $domain est conforme aux exigences."
-            }
-    }    
-    if ($nbrAccountNE -gt "0")
-        {
-        if ($mailreport -eq true)
-             {
-             Mail "[$env][$domain] Compte(s) en Never-Expire" "Un ou plusieurs compte(s) on(t) l'option Never-Expire d'activée(s) ce qui n'est pas recommandé, merci d'y remédier. Voir la pièce jointe pour plus de détails"
-             }
         }
-        Add-Content $logfile "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] ### Fin du script Check_Accounts-NeverExpire.ps1 - $nbrAccount comptes locaux ###"
-        Write-Host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] ### Fin du script Check_Accounts-NeverExpire.ps1 - $nbrAccount comptes locaux ###"
+}    
+if ($nbrAccountNE -gt "0")
+{
+    if ($mailreport -eq true)
+    {
+        Mail "[$env][$domain] Compte(s) en Never-Expire" "Un ou plusieurs compte(s) on(t) l'option Never-Expire d'activée(s) ce qui n'est pas recommandé, merci d'y remédier. Voir la pièce jointe pour plus de détails"
+    }
+}
+Add-Content $logfile "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] ### Fin du script Check_Accounts-NeverExpire.ps1 - $nbrAccount comptes locaux ###"
+Write-Host "[$(Get-Date -Format "dd/MM/yyyy_HH:mm:ss")] ### Fin du script Check_Accounts-NeverExpire.ps1 - $nbrAccount comptes locaux ###"
