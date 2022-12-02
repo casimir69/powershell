c8d673845db71bf29104ae4c3cd80f45ee66a1d8
casimir69 <65390071+casimir69@users.noreply.github.com>
2022-12-02 22:07:58 +0100

    Update GPO_Backup.ps1

--- GPO_Backup.ps1
+++ GPO_Backup.ps1
@@ -1,26 +1,46 @@
 <#
 .DESCRIPTION
         Sauvegarde des GPO  
-        le script génère un fichier log
+        le script génère un backup de chacune des GPO existantes ainsi qu'un fichier flag avec la date de la sauvegarde.
 .NOTES
         Author: casimir69
 #>
 
-Import-Module ActiveDirectory
+#region ##### load module #####
+If ($null -eq (Get-Module -Name ActiveDirectory -ErrorAction SilentlyContinue))
+{
+    Try
+    {
+        Import-Module ActiveDirectory
+    }
+    Catch
+    {
+        Write-Error "Unable to load the module" -ErrorAction Continue
+        Write-Error $Error[1] -ErrorAction Continue
+        Exit 1
+    }
+}
+#endregion ##### load module #####
 
+#region ##### varaible #####
 [datetime]$date = Get-date -format dd-MM-yyyy
-[string]$GPOPath = “D:\GPOBackups\$date” #Définition du chemin de l'export
-$WMIPath = "D:\GPOBackups\$date\WMI_Full_Backup.csv" #Définition du fichier de sauvegarde des filtres WMI
+[string]$GPOPath = “D:\BACKUP\GPO\$date”               #Définition du chemin de l'export
+$WMIPath = "D:\BACKUP\GPO\$date\WMI_Full_Backup.csv"   #Définition du fichier de sauvegarde des filtres WMI
+#endregion ##### variable #####
 
+#region ##### main script #####
 Backup-Gpo -All -Path $GPOPath #Export des GPO
 
-Get-ADObject -Filter 'objectClass -eq "msWMI-Som"' -Properties "msWMI-Name","msWMI-Parm1","msWMI-Parm2"| Select-Object "msWMI-Name","msWMI-Parm1","msWMI-Parm2"|export-csv $WMIPath -notypeinformation -delimiter ";" #Export des informations sur les filtres WMI
+Get-ADObject -Filter 'objectClass -eq "msWMI-Som"' -Properties "msWMI-Name","msWMI-Parm1","msWMI-Parm2"| Select-Object "msWMI-Name","msWMI-Parm1","msWMI-Parm2"|export-csv $WMIPath -notypeinformation -delimiter ";"   #Export des informations sur les filtres WMI
 
 if (Test-Path "$GPOPath\_backuped_*") #Test de l'existance du flag
 {
-    Get-Item "$GPOPath\_backuped_*" |Rename-Item -NewName "_backuped_$date.txt" #S'il existe, modification de son nom avec la date du jour
+    #S'il existe, modification de son nom avec la date du jour
+    Get-Item "$GPOPath\_backuped_*" |Rename-Item -NewName "_backuped_$date.txt"
 }
 else 
 {
-    New-Item "$GPOPath\_backuped_$date.txt" -type file #S'il n'existe pas, création du flag
+    #S'il n'existe pas, création du flag
+    New-Item "$GPOPath\_backuped_$date.txt" -type file
 }
+#endregion ##### main script #####
